ba8797be32469067b360d3539c2fb13e954ff169
Assaf <assafo@gmail.com>
2021-08-15 09:13:46 +0530

    add context and child component example

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # truffle-unbox-react-hooks
+
+A starter kit for Dapps using Truffle and React Hooks.
+
+To run:
+truffle develop
+migrate --reset
+
+In another console:
+cd client
+npm start
--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./getWeb3";
+import BlockchainContext from "./BlockchainContext.js";
+import ChildComponent from "./ChildComponent";
 
 import "./App.css";
 
@@ -68,19 +70,24 @@ function App() {
     return <div>Loading Web3, accounts, and contract...</div>;
   }
   return (
-    <div className="App"> 
-    <h1>Good to Go!</h1>
-    <p>Your Truffle Box is installed and ready.</p>
-    <h2>Smart Contract Example</h2>
-    <p>
-      If your contracts compiled and migrated successfully, below will show
-      a stored value of 5 (by default).
-    </p>
-    <p>
-      Try changing the value stored on <strong>line 42</strong> of App.js.
-    </p>
-    <div>The stored value is: {storageValue}</div>
-  </div>
+    <div className="App">
+      <BlockchainContext.Provider value={{web3, accounts, contract}}>
+        <h1>Good to Go!</h1>
+        <p>Your Truffle Box is installed and ready.</p>
+        <h2>Smart Contract Example</h2>
+        <p>
+          If your contracts compiled and migrated successfully, below will show
+          a stored value of 5 (by default).
+        </p>
+          <ChildComponent>
+
+          </ChildComponent>
+        <p>
+          Try changing the value stored on <strong>line 42</strong> of App.js.
+        </p>
+        <div>The stored value is: {storageValue}</div>
+      </BlockchainContext.Provider>
+    </div>
   )
 }
 
